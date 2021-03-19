3b38f4de19b8fdf2a07d47bd7fc5487624fa96da
Galym <62488119+Galymjohn@users.noreply.github.com>
2021-03-18 20:05:07 -0500

    blinking lights and sounded added

--- game.js
+++ game.js
@@ -1,13 +1,19 @@
-//new array
+
 var buttonColours = ["red", "blue", "green", "yellow"];
-//array for gamememory
+
 var gamePattern = [];
-//function for random number genirator.
-function nextSequene(){
 
-  var randomNumber = Math.floor(Math.random()*4);
+function nextSequence() {
 
-  randomChosenColour = buttonColours[randomNumber];
-
+  var randomNumber = Math.floor(Math.random() * 4);
+  var randomChosenColour = buttonColours[randomNumber];
   gamePattern.push(randomChosenColour);
+
+  //1. Use jQuery to select the button with the same id as the randomChosenColour
+  //2. Use Google/Stackoverflow to figure out how you can use jQuery to animate a flash to the button selected in step 1.
+  $("#" + randomChosenColour).fadeIn(100).fadeOut(100).fadeIn(100);
+
+  //3. Use Google/Stackoverflow to figure out how you can use Javascript to play the sound for the button colour selected in step 1.
+  var audio = new Audio("sounds/" + randomChosenColour + ".mp3");
+  audio.play();
 }
